3364ec2c73003a66855267f36de31ca030ed85d5
sunyongjie1984 <sunyongjie1984@gmail.com>
2016-01-13 19:10:05 +0800

    ^_^ sorry this is a test again

--- isll/islln.h
+++ isll/islln.h
@@ -11,12 +11,13 @@ class islln
     // and the class isll can access them
     // and the global function overloaded operators can access them.
     // ^_^ and I add this line just for test the git tool, sorry ha ha ha
+    // ^_^ this is a test again, I am very sorry for testing git functions
     friend class isll;
     friend ostream& operator<<(ostream&, const isll&);
     friend istream& operator>>(istream&, isll&);
 public:
     islln(): info(0), next(nullptr) { }
-    islln(int const & i, islln* const p_next = NULL): info(i), next(p_next) { }
+    islln(int const & i, islln* const p_next = NULL) : info(i), next(p_next) { }
 private:
     int info;
     islln* next;
